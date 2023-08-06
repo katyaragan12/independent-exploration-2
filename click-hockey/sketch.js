2448d268f23e246bd32dfd507c26a68b9ed845d1
katyaragan12 <140009732+katyaragan12@users.noreply.github.com>
2023-08-06 14:54:25 -0500

    Update sketch.js

--- click-hockey/sketch.js
+++ click-hockey/sketch.js
@@ -1,14 +1,20 @@
+
+
+
+//click p to pause the game
+
+
+
 let xPos = 250;
 let yPos = 0;
 let xSpeed = 2;
 let ySpeed = 2;
 let score = 0;
+let isPaused = false; // Initialize game state as not paused
 
 function setup() {
     createCanvas(500, 500);
-
     noStroke();
-
     rectMode(CENTER);
 }
 
@@ -16,31 +22,33 @@ function draw() {
     background(222);
 
     // draw goal
-    fill(0, 255 0);
+    fill(0, 255, 0);
     rect(250, 490, 60, 20);
 
     // draw ball
     fill(255, 0, 255);
-    rect(30, xPos, yPos);
+    rect(xPos, yPos, 30, 30);
 
     // draw score
     fill(18);
     textSize(20);
-    text("Score: " + score, 0, 0)
+    text("Score: " + score, 10, 25);
 
     // check if in goal
-    if (xPos + 15 <= 220 && xPos - 15 <= 280 && yPos + 15 >= 480) {
-        score;
+    if (!isPaused && xPos >= 220 && xPos <= 280 && yPos >= 480) {
+        score++;
         // reset position and speed
         xPos = random(15, 485);
         yPos = 0;
         xSpeed = random(-5, 5);
-        ySpeed = random(0, 10);
+        ySpeed = random(3, 8); // Change the range to make the ball go down
     }
 
-    // move forward
-    xPos += xSpeed;
-    yPos += ySpeed;
+    // move forward if not paused
+    if (!isPaused) {
+        xPos += xSpeed;
+        yPos += ySpeed;
+    }
 
     // wrap around if boundary exceeded
     if (xPos < 0) {
@@ -57,5 +65,12 @@ function draw() {
 
 function mouseClicked() {
     xSpeed = random(-5, 5);
-    ySpeed = random(0, 5);
+    ySpeed = random(3, 8); // Change the range to make the ball go down
+}
+
+function keyPressed() {
+    // Check if the "P" key is pressed to toggle pause
+    if (key === 'p' || key === 'P') {
+        isPaused = !isPaused;
+    }
 }
